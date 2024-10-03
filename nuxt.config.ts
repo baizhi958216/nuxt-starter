815c1a956eb48508cdc3b1be386033db744be707
baizhi958216 <1475289190@qq.com>
2024-10-04 07:35:44 +0800

    feat: Enable nuxt 4 features
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-10-03",
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    defaults: {
+      useAsyncData: {
+        deep: true,
+      },
+    },
+  },
+
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false,
+    },
+  },
+
+  devtools: { enabled: true },
+});
