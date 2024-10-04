10afaa69378dea01edbd8096475c2ad3deb29b39
baizhi958216 <1475289190@qq.com>
2024-10-04 08:01:55 +0800

    feat: Config tailwindcss
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="container font-Montserrat">
+    <h1 class="text-lg text-blue-500">Hello</h1>
   </div>
 </template>
