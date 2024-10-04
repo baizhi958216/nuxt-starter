f1a5083312bbf58f71cc81d6905f8af3de6eb117
baizhi958216 <1475289190@qq.com>
2024-10-04 08:46:57 +0800

    feat: Define BaseNavigation
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="container font-Montserrat">
-    <h1 class="text-lg text-blue-500">Hello</h1>
+  <div class="font-Montserrat">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Home page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+// definePageMeta({ layout: "login" });
+</script>
+
+<style scoped></style>
